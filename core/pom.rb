7e2fbbb588992031f2350ceb03b4ce343e688897
David Roetzel <david@roetzel.de>
2017-02-05 21:30:56 +0100

    Downgrade jnr-constants.
    As part of #4004 in order to work around #4006.

--- core/pom.rb
+++ core/pom.rb
@@ -47,7 +47,7 @@ project 'JRuby Core' do
   jar 'com.github.jnr:jnr-x86asm:1.0.2', :exclusions => ['com.github.jnr:jnr-ffi']
   jar 'com.github.jnr:jnr-unixsocket:0.15', :exclusions => ['com.github.jnr:jnr-ffi']
   jar 'com.github.jnr:jnr-posix:3.0.35-SNAPSHOT', :exclusions => ['com.github.jnr:jnr-ffi']
-  jar 'com.github.jnr:jnr-constants:0.9.6', :exclusions => ['com.github.jnr:jnr-ffi']
+  jar 'com.github.jnr:jnr-constants:0.9.1', :exclusions => ['com.github.jnr:jnr-ffi']
   jar 'com.github.jnr:jnr-ffi:2.1.2'
   jar 'com.github.jnr:jffi:${jffi.version}'
   jar 'com.github.jnr:jffi:${jffi.version}:native'
